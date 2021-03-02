18fc2cb2ecd3a3215d5c822189936109ada52c9c
julian-medve <julian.medve.superdev@gmail.com>
2021-03-02 10:03:36 +0900

    Quality problem solved

--- CreateEnvironmentGraphics.jsx
+++ CreateEnvironmentGraphics.jsx
@@ -12,7 +12,6 @@ var primaryColor = prompt ("Please enter primary color with HEX value ", "0000ff
 var logoFile = File.openDialog ("Please select logo file", "*.*", false);
 var outputPath = Folder.selectDialog("Choose folder to save files");
 
-
 // var logoFile = new File('~/Downloads/Sample_Logo.png');
 // var primaryColor = "0000ff";
 // var outputPath = "~/Downloads";
@@ -22,6 +21,35 @@ if (! outputFolder.exists ) {
  outputFolder.create();
 }
 
+
+//Create TOP BANNERS
+Create(2560,50, "topBanner-2560px.png", "MiddleLeft", 10)
+Create(2208,50, "topBanner-2208w.png", "MiddleLeft", 10)
+Create(2048,50, "topBanner-2048w.png", "MiddleLeft", 10)
+Create(2048,50, "topBanner-2048px.png", "MiddleLeft", 10)
+Create(1920,50, "topBanner-1920w.png", "MiddleLeft", 10)
+Create(1536,50, "topBanner-1536w.png", "MiddleLeft", 10)
+Create(1440,50, "topBanner-1440px.png", "MiddleLeft", 10)
+Create(1280,50, "topBanner-1280px.png", "MiddleLeft", 10)
+Create(1242,50, "topBanner-1242w.png", "MiddleLeft", 10)
+Create(1125,50, "topBanner-1125w.png", "MiddleLeft", 10)
+Create(1080,50, "topBanner-1080w.png", "MiddleLeft", 10)
+Create(1024,50, "topBanner-122h.png", "MiddleLeft", 10)
+Create(1024,50, "topBanner-1024px.png", "MiddleLeft", 10)
+Create(1024, 50, "topBanner-1024w.png", "MiddleLeft", 10)
+Create(854, 50, "topBanner-854px.png", "MiddleLeft", 10)
+Create(828,50, "topBanner-828px.png", "MiddleLeft", 10)
+Create(828,50, "topBanner-828w.png", "MiddleLeft", 10)
+Create(800,50, "topBanner-800px.png", "MiddleLeft", 10)
+Create(768,50, "topBanner-768w.png", "MiddleLeft", 10)
+Create(750, 50, "topBanner-750w.png", "MiddleLeft", 10)
+Create(720,50, "topBanner-720w.png", "MiddleLeft", 10)
+Create(640,50, "topBanner-640w.png", "MiddleLeft", 10)
+Create(640,50, "topBanner-320w-640px.png", "MiddleLeft", 10)
+Create(600,50, "topBanner-600px.png", "MiddleLeft", 10)
+Create(569,50, "topBanner-569px.png", "MiddleLeft", 10)
+Create(480,50, "topBanner-480px.png", "MiddleLeft", 10)
+Create(480,50, "topBanner-480w.png", "MiddleLeft", 10)
 Create(320,50, "topBanner-320px.png", "MiddleLeft", 10)
 Create(2732,50, "topBanner-2732w.png", "MiddleLeft", 10)
 Create(2732,50, "topBanner-273px.png", "MiddleLeft", 10)
--- functions.jsx
+++ functions.jsx
@@ -73,6 +73,7 @@ function Create(width, height, outputName, logoPosition, logoPadding){
   app.preferences.rulerUnits = Units.PIXELS;
   docRef.resizeCanvas (width, height);
 
+
   // Add Background layer
   var layerRef = docRef.artLayers.add();
 
@@ -89,11 +90,10 @@ function Create(width, height, outputName, logoPosition, logoPadding){
   docRef.selection.fill( myColor); //fills background layer with primary.
 
 
-
   // Get logo layer size
   var logoLayer = docRef.artLayers[0];
-  var logoWidth = logoLayer.bounds[2]-logoLayer.bounds[0]; //Grab the length
-  var logoHeight = logoLayer.bounds[3]-logoLayer.bounds[1]; //Grab the width
+  var logoWidth = logoLayer.bounds[2]-logoLayer.bounds[0]; 
+  var logoHeight = logoLayer.bounds[3]-logoLayer.bounds[1]; 
 
   // Remove pixels from the length/width "200 px" => "200"
   logoWidth = logoWidth.toString().replace(' px', '');
@@ -101,11 +101,24 @@ function Create(width, height, outputName, logoPosition, logoPadding){
 
 
   // Resize Logo for height
-  if(logoHeight > height){
-    var changeHeight = height / logoHeight * 100;
-    var changeWidth = ( height / logoHeight * logoWidth) / logoWidth * 100;
-    logoLayer.resize(changeWidth, changeHeight, AnchorPosition.MIDDLECENTER);
+  if(logoWidth > width || logoHeight > height){
 
+    var ratioWidth = logoWidth / width;
+    var ratioHeight = logoHeight / height;
+    var changeWidth, changeHeight;
+
+    if(ratioHeight > ratioWidth){
+      changeHeight = height / logoHeight * 100;
+      changeWidth = ( height / logoHeight * logoWidth) / logoWidth * 100;
+    }
+    else{
+      // Resize Logo for width
+      changeWidth = width / logoWidth * 100;
+      changeHeight = ( width / logoWidth * logoHeight) / logoHeight * 100;
+    }
+
+    logoLayer.resize(changeWidth, changeHeight, AnchorPosition.MIDDLECENTER);
+  
     // Recalculate resized logo size
     logoWidth = logoLayer.bounds[2]-logoLayer.bounds[0]; //Grab the length
     logoHeight = logoLayer.bounds[3]-logoLayer.bounds[1]; //Grab the width
@@ -113,29 +126,15 @@ function Create(width, height, outputName, logoPosition, logoPadding){
     logoHeight = logoHeight.toString().replace(' px', '');
   }
 
-  // Resize Logo for width
-  if(logoWidth > width){
-    var changeWidth = width / logoWidth * 100;
-    var changeHeight = ( width / logoWidth * logoHeight) / logoHeight * 100;
-    logoLayer.resize(changeWidth, changeHeight, AnchorPosition.MIDDLECENTER);
-
-    // Recalculate resized logo size
-    logoWidth = logoLayer.bounds[2]-logoLayer.bounds[0]; //Grab the length
-    logoHeight = logoLayer.bounds[3]-logoLayer.bounds[1]; //Grab the width
-    logoWidth = logoWidth.toString().replace(' px', '');
-    logoHeight = logoHeight.toString().replace(' px', '');
-  }
-
- 
   
   if(logoPosition.indexOf("Left") != -1)
-    logoLayer.translate((width / 2 - logoWidth / 2 - logoPadding) * (-1), 0);
+    logoLayer.translate(Math.round(width / 2 - logoWidth / 2 - logoPadding) * (-1), 0);
   if(logoPosition.indexOf("Right") != -1)
-    logoLayer.translate(width / 2 - logoWidth / 2 - logoPadding, 0);
+    logoLayer.translate(Math.round(width / 2 - logoWidth / 2 - logoPadding), 0);
   if(logoPosition.indexOf("Top") != -1)
-    logoLayer.translate(0, (height / 2 - logoHeight / 2 - logoPadding) * (-1));
+    logoLayer.translate(0, Math.round(height / 2 - logoHeight / 2 - logoPadding) * (-1));
   if(logoPosition.indexOf("Bottom") != -1)
-    logoLayer.translate(0, height / 2 - logoHeight / 2 - logoPadding);
+    logoLayer.translate(0, Math.round(height / 2 - logoHeight / 2 - logoPadding));
 
   
   if(outputName.indexOf("PNG") != -1 || outputName.indexOf("png") != -1)
